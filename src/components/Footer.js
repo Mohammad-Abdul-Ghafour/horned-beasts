63f8ee962a4ea2b0f0e7c6dbf51828fbf08458a1
Mohammad Abdul Ghafour <mohammad1478971539@gmail.com>
2021-09-12 17:09:05 +0300

    Finishing Lab 01

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+class Footer extends React.Component{
+    render(){
+        return(
+            <h3>Auther : Mohammad Abdul Ghafour</h3>
+        )
+    }
+}
+export default Footer;

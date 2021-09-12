8856c71a7b67bf235b6d288672acb3820aa71533
Mohammad Abdul Ghafour <mohammad1478971539@gmail.com>
2021-09-12 19:25:03 +0300

    Strech Goal

--- src/components/HornedBeasts.js
+++ src/components/HornedBeasts.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 class Hornedbeast extends React.Component {
     render() {
+        // console.log(this.props)
         return (
             <div>
                 <h2>{this.props.title}</h2>

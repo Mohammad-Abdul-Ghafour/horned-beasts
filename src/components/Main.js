0490cec3ea4649c6869c48cd7c520910a6ee188b
Mohammad Abdul Ghafour <mohammad1478971539@gmail.com>
2021-09-15 15:03:10 +0300

    Finishing lab 04

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,13 +2,15 @@ import React from 'react';
 import Hornedbeast from './HornedBeasts';
 import './Main.css';
 import Selectedbeast from './SelectedBeast';
+import Formmodal from './FormModal';
 
 class Main extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             show: false,
-            selectedBeast : {}
+            selectedBeast: {},
+            numberOfHornes: 0
         };
     }
 
@@ -18,17 +20,36 @@ class Main extends React.Component {
         });
     }
 
+    handleFilter = (numberOfHornes) => {
+
+
+        this.setState({
+            numberOfHornes: numberOfHornes
+        });
+
+    }
+
     handleSelectedBeast = (title) => {
         console.log(title);
         let selectedBeast = this.props.beasrArray.find(element => element.title === title);
         console.log(selectedBeast);
-        this.setState({selectedBeast : selectedBeast});
+        this.setState({ selectedBeast: selectedBeast });
     }
 
 
     render() {
         let beasrArray = this.props.beasrArray;
-        let newArray = beasrArray.map((element, index) => {
+        let filterArray = beasrArray.filter(element =>
+        // element.horns === this.state.numberOfHornes
+        {
+            if (this.state.numberOfHornes === 0) {
+                return element;
+            } else {
+                return element.horns === this.state.numberOfHornes;
+            }
+        }
+        );
+        let newArray = filterArray.map((element, index) => {
             return (<Hornedbeast
                 handleShow={this.handleShow}
                 handleSelectedBeast={this.handleSelectedBeast}
@@ -41,10 +62,13 @@ class Main extends React.Component {
         });
 
         return (
-            <div id='container'>
-                {newArray}
+            <>
+                <Formmodal handleFilter={this.handleFilter} />
+                <div id='container'>
+                    {newArray}
+                </div>
                 <Selectedbeast showState={this.state} onHide={this.handleShow} />
-            </div>
+            </>
         );
 
 
